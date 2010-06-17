3aad45c7abde285b4bec11c9029b234b8cac862b
Patrick Berryhill <pjberry@gmail.com>
2010-06-17 05:48:30 -0500

    first commit

---
+++ schema-rede/src/test/java/com/bitmotif/schemarede/ConnectionTest_DBUT.java
@@ -0,0 +1,38 @@
+package com.bitmotif.schemarede;
+
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pjberry
+ * Date: Jun 16, 2010
+ * Time: 8:29:35 PM
+ */
+public class ConnectionTest_DBUT {
+
+   @Test
+   public void testConnection() throws Exception {
+
+      try {
+         Class.forName ("oracle.jdbc.driver.OracleDriver");
+      } catch (ClassNotFoundException e) {
+         e.printStackTrace();
+      }
+
+      Connection conn = DriverManager.getConnection
+         ("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "a");
+      // @machineName:port:SID,   userid,  password
+
+      Statement stmt = conn.createStatement();
+      ResultSet rset = stmt.executeQuery("select BANNER from SYS.V_$VERSION");
+      while (rset.next())
+         System.out.println ("********** " + rset.getString(1));   // Print col 1
+      stmt.close();
+
+   }
+}
