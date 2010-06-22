a56d94394ba8b64f918b7a3ded4234ac5d1d0004
Patrick Berryhill <pjberry@gmail.com>
2010-06-22 05:35:41 -0500

    a little more clean up

---
+++ schema-rede/src/test/java/com/bitmotif/schemarede/jdbc/AbstractDatabaseTestCase.java
@@ -0,0 +1,29 @@
+package com.bitmotif.schemarede.jdbc;
+
+import org.junit.After;
+import org.junit.Before;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pjberry
+ * Date: Jun 22, 2010
+ * Time: 5:29:51 AM
+ */
+public class AbstractDatabaseTestCase {
+
+   protected Connection conn;
+
+   @Before
+   public void createConnection() throws Exception {
+      Class.forName ("oracle.jdbc.driver.OracleDriver");
+      conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "a");
+   }
+
+   @After
+   public void closeConnection() throws Exception {
+      conn.close();
+   }
+}
