be79eda435af2cb5ae80b1735652a847ca24bc16
Patrick Berryhill <pjberry@gmail.com>
2010-06-22 05:36:03 -0500

    a little more clean up

--- schema-rede/src/test/java/com/bitmotif/schemarede/ConnectionTest_DBUT.java
+++ schema-rede/src/test/java/com/bitmotif/schemarede/ConnectionTest_DBUT.java
@@ -1,12 +1,11 @@
 package com.bitmotif.schemarede;
 
-import org.junit.Test;
+import com.bitmotif.schemarede.jdbc.AbstractDatabaseTestCase;
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.DriverManager;
-import java.sql.Connection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,12 +13,10 @@ import java.sql.Connection;
  * Date: Jun 16, 2010
  * Time: 8:29:35 PM
  */
-public class ConnectionTest_DBUT {
+public class ConnectionTest_DBUT extends AbstractDatabaseTestCase {
 
    @Test
    public void testConnection() throws Exception {
-      Class.forName ("oracle.jdbc.driver.OracleDriver");
-      Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "a");
       Statement stmt = conn.createStatement();
       ResultSet resultSet = stmt.executeQuery("select 1 from dual");
 
